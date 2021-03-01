7e92fcddd84baa309af52c8a086b66cd05912387
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-03-01 20:07:34 -0300

    Update bbbot21.py
    atualiza a página quando dá ruim

--- bbbot21.py
+++ bbbot21.py
@@ -3,7 +3,7 @@ from time import sleep
 from PIL import Image
 from random import *
 
-brother = Image.open('nego_di.png')
+brother = Image.open('lumena.png')
 capt = Image.open('capt.png')
 vot =  Image.open('vot_again.png')
 
@@ -11,28 +11,41 @@ comment = 1
 
 mouse_func = [pyautogui.easeOutBack, pyautogui.easeInOutQuad, pyautogui.easeOutQuad, pyautogui.easeInBounce, pyautogui.easeInElastic]
 
+
 def voto():
     sleep(1)
     w, z = pyautogui.locateCenterOnScreen(capt, confidence=0.8)
     pyautogui.moveTo(w+randint(-20,150), z+randint(-30,10), uniform(0.5, 2.0), choice(mouse_func))
     pyautogui.click()
     sleep(2)
-    a, b = pyautogui.locateCenterOnScreen(vot, confidence=0.8)
-    pyautogui.moveTo(a+randint(-250,250), b+randint(-15,15), uniform(0.5, 2.0), choice(mouse_func))
-    pyautogui.click()
-    sleep(1)
+    try:
+        a, b = pyautogui.locateCenterOnScreen(vot, confidence=0.8)
+        pyautogui.moveTo(a+randint(-250,250), b+randint(-15,15), uniform(0.5, 2.0), choice(mouse_func))
+        pyautogui.click()
+        sleep(1)
+    except:
+        pyautogui.press('f5')
+        sleep(2)
+        pyautogui.scroll(-200)
+
+def irmao():
+    try:
+        pyautogui.scroll(-300)
+        sleep(1)
+        x, y = pyautogui.locateCenterOnScreen(brother, confidence=0.8)
+        pyautogui.moveTo(x+randint(-50,300), y+randint(-5,5), uniform(0.5, 2.0), choice(mouse_func))
+        pyautogui.click()
+        sleep(1)
+        if pyautogui.locateCenterOnScreen(vot, confidence=0.8) == None:
+            pyautogui.scroll(-400)
+            voto()
+        else:
+            voto()
+    except:
+        pyautogui.press('f5')
 
 
 while True:
-    pyautogui.scroll(-100)
-    x, y = pyautogui.locateCenterOnScreen(brother, confidence=0.8)
-    pyautogui.moveTo(x+randint(-80,300), y+randint(-15,15), uniform(0.5, 2.0), choice(mouse_func))
-    pyautogui.click()
-    sleep(1)
-    if pyautogui.locateCenterOnScreen(vot, confidence=0.8) == None:
-        pyautogui.scroll(-300)
-        voto()
-    else:
-        voto()
+    irmao()
     comment += 1
     print("{} votos computados".format(comment))
